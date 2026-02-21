446be5d483033ee84b11e75dc30737204236908b
Arvydas <arvydasv201@gmail.com>
2026-02-21 21:26:31 +0200

    translation fixes

--- Source/SimpleRecycleMod.cs
+++ Source/SimpleRecycleMod.cs
@@ -30,9 +30,9 @@ namespace AVSR
         {
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
-            listingStandard.CheckboxLabeled("Recycle efficiency based on apparel durability".Translate(), ref Settings.isDurabilityInfluenced);
+            listingStandard.CheckboxLabeled("SimpleRecycle_RecycleEfficiencyBasedOnApparelDurability".Translate(), ref Settings.isDurabilityInfluenced);
             listingStandard.Gap(8);
-            listingStandard.Label("Recycle efficiency".Translate() + ": " + Settings.efficiency);
+            listingStandard.Label("SimpleRecycle_RecycleEfficiency".Translate() + ": " + Settings.efficiency);
             Settings.efficiency = Mathf.Round(listingStandard.Slider(Settings.efficiency, 0.0f, 1.0f) * 100) / 100.0f;
             listingStandard.End();
             base.DoSettingsWindowContents(inRect);
